a1f7734b5a4de003d782f33920b1f7ff9ef0aadf
Nazarchik629 <solodovnikov.nazarchik.s@gmail.com>
2026-03-27 12:01:42 +0500

    Add student name display to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="7 чудес Казахстана", layout="wide"
 
 st.markdown("<h1 style='text-align: center;'>KZ Семь Чудес Казахстана</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>Меңің Отаныма-меңің бастамам</h2>", unsafe_allow_html=True)
+st.write("Ученика Солодовникова Назара КГУ ОШ №125")
 st.write("---")
 
 # Список картинок
